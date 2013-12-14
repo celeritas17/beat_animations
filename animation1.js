d5614d15b11992364d44866fa9430bad1911eca9
Ryan Koven <celeritas@ucla.edu>
2013-12-13 20:44:27 -0800

    Reorganize

---
+++ animation1.js
@@ -0,0 +1,24 @@
+var gallery_bar = beat_bar("gallery", 500);
+var example_bar = beat_bar("example", 500);
+
+var randomize_example = randomize_color(1000, "example");
+
+var randomize_gallery = randomize_color(1000, "gallery");
+
+var oscillate_gallery = oscillate("gallery", "width", 200, 500, 20);
+var oscillate_example = oscillate("example", "width", 200, 500, 20);
+
+var div1_bar = beat_bar("div1", 500);
+var div2_bar = beat_bar("div2", 500);
+
+var toggle_div1 = toggle_color(1000, "div1", "blue");
+var toggle_div2 = toggle_color(1000, "div2", "blue");
+var div3_bar = beat_bar("div3", 500);
+var div4_bar = beat_bar("div4", 500);
+
+var toggle_div3 = toggle_color(1000, "div3", "blue");
+var toggle_div4 = toggle_color(1000, "div4", "blue");
+var div5_bar = beat_bar("div5", 500);
+var randomize_div5 = randomize_color(1000, "div5");
+var div6_bar = beat_bar("div6", 500);
+var randomize_div6 = randomize_color(1000, "div6");
